32ae4e2398c9d6003d6d2959377a4d0960282dae
pewbert <mercenary.kid@gmail.com>
2026-03-30 10:38:45 -0400

    Update TPM-stat.ps1

--- TPM-stat.ps1
+++ TPM-stat.ps1
@@ -1,4 +1,4 @@
-﻿# For use with Windows 11
+# For use with Windows 11
 # https://learn.microsoft.com/en-us/windows/win32/secprov/GetPhysicalPresenceRequest-win32-tpm
 # https://learn.microsoft.com/en-us/windows/win32/secprov/SetPhysicalPresenceRequest-win32-tpm
 # https://learn.microsoft.com/en-us/windows/win32/secprov/GetPhysicalPresenceTransition-win32-tpm
@@ -11,6 +11,8 @@
 #We check to see the PhysicalPresenceTransition value, 1 or 2 means a reboot is required to clear the Request state.
 #We check to see the PhysicalPresenceConfirmationStatus value, this checks to see if the feature can be cleared or not with a physically present person or if it is blocked or supported by the O/S and/or BIOS
 
+if (([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator) -eq $False) { write-host "Need Administrative privileges to continue..."}
+else {
 
 $table = New-Object System.Data.DataTable
 
@@ -26,64 +28,67 @@ $row1 = $table.NewRow()
 $row2 = $table.NewRow()
 $row3 = $table.NewRow()
 
-$tval = Get-CimInstance -Namespace 'root/cimv2/Security/MicrosoftTpm' -ClassName 'Win32_TPM' | Invoke-CimMethod -MethodName 'GetPhysicalPresenceTransition'
+$tpm = Get-CimInstance -Namespace 'root/cimv2/Security/MicrosoftTpm' -ClassName 'Win32_TPM'
+$tval = $tpm | Invoke-CimMethod -MethodName 'GetPhysicalPresenceTransition'
+$rval = $tpm | Invoke-CimMethod -MethodName 'GetPhysicalPresenceRequest'
+$x    = $tpm | Invoke-CimMethod -MethodName 'GetPhysicalPresenceConfirmationStatus' -Arguments @{Operation=$rval.Request}
+
+$row1.ID = $rval.Request
+$row1.Instance = "GetPhysicalPresenceRequest"
 $row2.ID = $tval.Transition
 $row2.Instance = "GetPhysicalPresenceTransition"
+$row3.ID = $x.ConfirmationStatus
+$row3.Instance = "GetPhysicalPresenceConfirmationStatus"
+
     switch ($tval.Transition)
     {
-        "0" { $row2.Value = "No user action is needed to perform a TPM physical presence operation." }
-        "1" { $row2.Value = "To perform a TPM physical presence operation, the user must shutdown the computer and then turn it back on by using the power button. The user must be physically present at the computer to accept or reject the change when prompted by the BIOS." }
-        "2" { $row2.Value = "To perform a TPM physical presence operation, the user must restart the computer by using a warm reboot. The user must be physically present at the computer to accept or reject the change when prompted by the BIOS." }
-        "3" { $row2.Value = "The required user action is unknown." }
+        0 { $row2.Value = "No user action is needed to perform a TPM physical presence operation." }
+        1 { $row2.Value = "To perform a TPM physical presence operation, the user must shutdown the computer and then turn it back on by using the power button. The user must be physically present at the computer to accept or reject the change when prompted by the BIOS." }
+        2 { $row2.Value = "To perform a TPM physical presence operation, the user must restart the computer by using a warm reboot. The user must be physically present at the computer to accept or reject the change when prompted by the BIOS." }
+        3 { $row2.Value = "The required user action is unknown." }
         default {$row2.Value = "Not Implemented."  }
     }
 
-
-$rval = Get-CimInstance -Namespace 'root/cimv2/Security/MicrosoftTpm' -ClassName 'Win32_TPM' | Invoke-CimMethod -MethodName 'GetPhysicalPresenceRequest'
-$row1.ID = $rval.Request
-$row1.Instance = "GetPhysicalPresenceRequest"
 $rp = Get-TPM | Select-Object RestartPending
 if (($rp.RestartPending) -eq $True) 
  {
     switch ($rval.Request) 
     {
-            "0"  { $row1.Value = "No Request." }
-            "1"  { $row1.Value = "Enable the TPM." }
-            "2"  { $row1.Value = "Disable the TPM." }
-            "3"  { $row1.Value = "Activate the TPM." }
-            "4"  { $row1.Value = "Deactivate the TPM." }
-            "5"  { $row1.Value = "Clear the TPM."  }
-            "6"  { $row1.Value = "Enable and activate the TPM." }
-            "7"  { $row1.Value = "Deactivate and disable the TPM." }
-            "8"  { $row1.Value = "Allow the installation of a TPM owner." }
-            "9"  { $row1.Value = "Prevent the installation of a TPM owner." }
-            "10" { $row1.Value = "Enable, activate, and allow the installation of a TPM owner." }
-            "11" { $row1.Value = "Deactivate, disable, and prevent the installation of a TPM owner." }
-            "12" { $row1.Value = "Deferred Physical PresenceunownedFieldUpgrade. Physical presence setting has been updated." }
-            "13" { $row1.Value = "Not Implemented" }
-            "14" { $row1.Value = "Clear, enable, and activate the TPM. " }
-            "15" { $row1.Value = "SetNoPPIProvision_False. Sets the provision that you must be physically presence to set the TPM." }
-            "16" { $row1.Value = "SetNoPPIProvision_True. Sets the provision that you don't need to be physically presence to set the TPM." }
-            "17" { $row1.Value = "SetNoPPIClear_False. Sets the provision that you must be physically presence to clear the TPM." }
-            "18" { $row1.Value = "SetNoPPIClear_True. Sets the provision that you don't need to be physically presence to clear the TPM." }
-            "19" { $row1.Value = "SetNoPPIMaintenance_False. Sets the provision that you must be physically presence to maintain the TPM." }
-            "20" { $row1.Value = "SetNoPPIMaintenance_True. Sets the provision that you don't need to be physically presence to maintain the TPM." }
-            "21" { $row1.Value = "Enable, activate, and clear the TPM." }
-            "22" { $row1.Value = "Enable, activate, and clear the TPM, and then enable and reactivate the TPM." }
-            default { $row1.Value = "Not Implemented" }
+            0  { $row1.Value = "No Request." }
+            1  { $row1.Value = "Enable the TPM." }
+            2  { $row1.Value = "Disable the TPM." }
+            3  { $row1.Value = "Activate the TPM." }
+            4  { $row1.Value = "Deactivate the TPM." }
+            5  { $row1.Value = "Clear the TPM."  }
+            6  { $row1.Value = "Enable and activate the TPM." }
+            7  { $row1.Value = "Deactivate and disable the TPM." }
+            8  { $row1.Value = "Allow the installation of a TPM owner." }
+            9  { $row1.Value = "Prevent the installation of a TPM owner." }
+            10 { $row1.Value = "Enable, activate, and allow the installation of a TPM owner." }
+            11 { $row1.Value = "Deactivate, disable, and prevent the installation of a TPM owner." }
+            12 { $row1.Value = "Deferred Physical PresenceunownedFieldUpgrade. Physical presence setting has been updated." }
+           #13 { $row1.Value = "Not Implemented." }
+            14 { $row1.Value = "Clear, enable, and activate the TPM. " }
+            15 { $row1.Value = "SetNoPPIProvision_False. Sets the provision that you must be physically presence to set the TPM." }
+            16 { $row1.Value = "SetNoPPIProvision_True. Sets the provision that you don't need to be physically presence to set the TPM." }
+            17 { $row1.Value = "SetNoPPIClear_False. Sets the provision that you must be physically presence to clear the TPM." }
+            18 { $row1.Value = "SetNoPPIClear_True. Sets the provision that you don't need to be physically presence to clear the TPM." }
+            19 { $row1.Value = "SetNoPPIMaintenance_False. Sets the provision that you must be physically presence to maintain the TPM." }
+            20 { $row1.Value = "SetNoPPIMaintenance_True. Sets the provision that you don't need to be physically presence to maintain the TPM." }
+            21 { $row1.Value = "Enable, activate, and clear the TPM." }
+            22 { $row1.Value = "Enable, activate, and clear the TPM, and then enable and reactivate the TPM." }
+            default { $row1.Value = "Not Implemented." }
      }
      
 }
-$x = Get-CimInstance -Namespace 'root/cimv2/Security/MicrosoftTpm' -ClassName 'Win32_TPM' | Invoke-CimMethod -MethodName 'GetPhysicalPresenceConfirmationStatus' -Arguments @{Operation=$rval.Request}
-$row3.ID = $x.ConfirmationStatus
-$row3.Instance = "GetPhysicalPresenceConfirmationStatus"
+else { $row1.Value = "No restart pending." }
     switch ($x.ConfirmationStatus) 
     {
-        "0" { $row3.Value = "Not Implemented." }
-        "1" { $row3.Value = "BIOS Only." }
-        "2" { $row3.Value = "Blocked for the OS by the BIOS cfg." }
-        "3" { $row3.Value = "Allowed and Physically Present user required."}
-        "4" { $row3.Value = "Allowed and Physically Present user not required."}
+        0 { $row3.Value = "Not Implemented." }
+        1 { $row3.Value = "BIOS Only." }
+        2 { $row3.Value = "Blocked for the OS by the BIOS cfg." }
+        3 { $row3.Value = "Allowed and Physically Present user required."}
+        4 { $row3.Value = "Allowed and Physically Present user not required."}
     }
 
 $table.Rows.Add($row1)
@@ -91,11 +96,4 @@ $table.Rows.Add($row2)
 $table.Rows.Add($row3)
 
 $table | Format-Table
-
-Function setPPR() #DO NOT USE -- BREAKS TPM and more
-{
-    $forceValue="0" #0 to 22 -- see GetPhysicalPresenceRequest row1 values
-    Get-CimInstance -Namespace 'root/cimv2/Security/MicrosoftTpm' -ClassName 'Win32_TPM' | Invoke-CimMethod -MethodName 'SetPhysicalPresenceRequest' -Arguments @{Request='$forceValue'}
-    Get-CimInstance -Namespace 'root/cimv2/Security/MicrosoftTpm' -ClassName 'Win32_TPM' | Invoke-CimMethod -MethodName 'GetPhysicalPresenceConfirmationStatus' -Arguments @{Operation=$rval.Request}
-    Get-CimInstance -Namespace 'root/cimv2/Security/MicrosoftTpm' -ClassName 'Win32_TPM' | Invoke-CimMethod -MethodName 'GetPhysicalPresenceResponse'
 }
